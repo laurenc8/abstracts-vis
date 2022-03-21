f3eafc6c9372fefda539056e376d3b8b536a5d5f
laurenc8 <laurenc8822@gmail.com>
2022-03-21 16:18:45 -0400

    delete vis.js and clean App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component }  from 'react';
-import { Button } from 'react-native';
-import SwipePage from './vis';
 
-  <Route exact path="/" component={SwipePage} />
+{/* <Route exact path="/" component={SwipePage} /> */}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        //<img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-
+      hi
     </div>
   );
 }
 
 export default App;
-function sayHello() {
-  alert('You clicked me!');
-}
-// Usage
-<button onClick={sayHello}>Default</button>;
