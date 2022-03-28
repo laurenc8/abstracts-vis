19ca748ba80a424794b6bd9eff89527fa6a4e99a
laurenc8 <laurenc8822@gmail.com>
2022-03-28 14:46:57 -0400

    slider in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Histogram from './components/histogram';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 function App() {
+  const [nBin, setnBin] = useState(30)
   const data = [{frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}, {frequency: 15}, {frequency: 20}, {frequency: 20}, {frequency: 250}, {frequency: 350}]
+  function valuetext(value) {
+    setnBin(value)
+  }
+  
   return (
     <div>
-      <Histogram data={data}/>
+      <Box sx={{ width: 300, margin: 5, marginBottom: 0 }}>
+        <Slider
+          aria-label="Number of Bins"
+          defaultValue={20}
+          getAriaValueText={valuetext}
+          valueLabelDisplay="on"
+          step={1}
+          marks
+          min={1}
+          max={50}
+        />
+      </Box>
+      <Histogram data={data} nBin={nBin}/>
     </div>
   );
 }
--- src/components/histogram.js
+++ src/components/histogram.js
@@ -2,7 +2,8 @@ import { useD3 } from '../hooks/useD3';
 import React from 'react';
 import * as d3 from 'd3';
 
-function Histogram({ data }) {
+function Histogram({ data, nBin}) {
+  console.log(nBin)
   var margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 500 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
@@ -26,56 +27,50 @@ function Histogram({ data }) {
       // A function that builds the graph for a specific value of bin
       function update(nBin) {
 
-      // set the parameters for the histogram
-      var histogram = d3.histogram()
+        // set the parameters for the histogram
+        var histogram = d3.histogram()
           .value(function(d) { return d.frequency; })   // I need to give the vector of value
           .domain(x.domain())  // then the domain of the graphic
           .thresholds(x.ticks(nBin)); // then the numbers of bins
 
-      // And apply this function to data to get the bins
-      var bins = histogram(data);
+        // And apply this function to data to get the bins
+        var bins = histogram(data);
 
-      // Y axis: update now that we know the domain
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-      yAxis
+        // Y axis: update now that we know the domain
+        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+        yAxis
           .transition()
           .duration(1000)
           .call(d3.axisLeft(y));
 
-      // Join the rect with the bins data
-      var u = svg.selectAll("rect")
+        // Join the rect with the bins data
+        var u = svg.selectAll("rect")
           .data(bins)
 
-      // Manage the existing bars and eventually the new ones:
-      u
+        // Manage the existing bars and eventually the new ones:
+        u
           .enter()
           .append("rect") // Add a new rect for each new elements
           .merge(u) // get the already existing elements as well
           .transition() // and apply changes to all of them
           .duration(1000)
-            .attr("x", 1)
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + (y(d.length)+10) + ")"; })
-            .attr("width", function(d) { return x(d.x1) - x(d.x0) - 4 ; })
-            .attr("height", function(d) { return height - y(d.length); })
-            .style("stroke", "#dcd0ff")
-            .style("stroke-width", "3px")
-            .style("fill", "none")
+          .attr("x", 1)
+          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + (y(d.length)+10) + ")"; })
+          .attr("width", function(d) { return x(d.x1) - x(d.x0) - 4 ; })
+          .attr("height", function(d) { return height - y(d.length); })
+          .style("stroke", "#dcd0ff")
+          .style("stroke-width", "3px")
+          .style("fill", "none")
 
 
-      // If less bar in the new histogram, I delete the ones not in use anymore
-      u
+        // If less bar in the new histogram, I delete the ones not in use anymore
+        u
           .exit()
           .remove()
 
       }
 
-      // Initialize with 20 bins
-      update(20)
-
-      // Listen to the button -> update if user change it
-      d3.select("#nBin").on("input", function() {
-        update(+this.value);
-      });
+      update(nBin)
     });
 
   return (
